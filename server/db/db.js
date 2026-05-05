a4bd48eb07cfa96f387e7d267f6eea14a720a232
Codewiizard <135721204+Codewiizard@users.noreply.github.com>
2026-05-05 14:51:14 +0530

    Use MONGO_URI environment variable

--- server/db/db.js
+++ server/db/db.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 
 
 export const connection = async()=>{
-    await mongoose.connect('mongodb+srv://Aashish:admin0159@cluster0.kdhkj2y.mongodb.net/')
+    const uri = process.env.MONGO_URI || 'mongodb+srv://Aashish:admin0159@cluster0.kdhkj2y.mongodb.net/';
+    await mongoose.connect(uri)
     .then(()=>console.log("Db connected"))
+    .catch((err)=>console.error("Db connection error:", err));
 }
