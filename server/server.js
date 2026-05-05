711a137a0ca6739fa08c0e7a63ef771419db4ed4
Codewiizard <135721204+Codewiizard@users.noreply.github.com>
2026-05-05 14:41:40 +0530

    Configure unified Vercel deployment

--- server/server.js
+++ server/server.js
@@ -25,6 +25,11 @@ connection();
 app.get('/',(req,res)=>{
     res.send('working')
 })
-app.listen(port, ()=>{
-    console.log(`Server started at ${port} `)
-})
+
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(port, ()=>{
+        console.log(`Server started at ${port} `)
+    })
+}
+
+export default app;
